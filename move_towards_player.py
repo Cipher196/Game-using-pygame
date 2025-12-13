e2a2a59582bdf925669b406fd2834187fd2a6b62
Ahshan Khan <73234686+Cipher196@users.noreply.github.com>
2025-12-13 12:02:36 +0530

    13/12  made game more smooth

--- move_towards_player.py
+++ move_towards_player.py
@@ -1,107 +1,144 @@
 import pygame
 import sys
 
-# -------------------- SETTINGS --------------------
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-FPS = 60
+# Settings
 
-# Colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+TILE_SIZE=20
 
-# -------------------- SETUP ------------------------
+grid = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,1],
+        [1,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,1,1,1,0,0,0,0,0,1],
+        [1,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,1,1,1,0,0,0,0,0,1],
+        [1,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,1,1,1,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],]
+
+Screen_Width=len(grid[0])*TILE_SIZE
+Screen_Height=len(grid)*TILE_SIZE
+
+FPS=60
+
+# Setup
+
 pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("My Pygame Game")
-clock = pygame.time.Clock()
+screen=pygame.display.set_mode((Screen_Width,Screen_Height))
+pygame.display.set_caption("Simple chase the player")
+clock=pygame.time.Clock()
 
-
-# ------------------- FUNCTION ------------------ 
-
-def collide(b1,b2):
-    
-    if b1.x + b1.w < b2.x  or b1.x > b2.x + b2.w:
-        return False
-    if b1.y + b1.h < b2.y or b1.y > b2.y + b2.h:
-        return False
-        
-    return True
-
-def enemy_move(enemy,player):
-    dx=player.x-enemy.x
-    dy=player.y-enemy.y
-
-    length=(dx**2+dy**2)**0.5
-    
-    if length!=0:
-        dx/=length
-        dy/=length
-
-    enemy.move(dx,dy)
-
-# -------------- CLASS ----------------- 
+# Class 
 
 class block:
 
-    def __init__(self,x,y,w,h,speed,color):
-        self.x=x 
+    def __init__(self,x,y,Size_Tiles,speed,color):
+        self.x=x
         self.y=y 
-        self.w=w 
-        self.h=h 
+        self.Size_Tiles=Size_Tiles
         self.speed=speed
         self.color=color
 
     def show(self):
-        pygame.draw.rect(screen,self.color,(self.x,self.y,self.w,self.h))
+        pygame.draw.rect(screen,self.color,(self.x*TILE_SIZE,self.y*TILE_SIZE,TILE_SIZE*self.Size_Tiles,TILE_SIZE*self.Size_Tiles))
 
     def move(self,x,y):
-        self.x += x*self.speed
-        self.y += y*self.speed
 
-        for block in blocks:
-            if collide(self,block):
-                self.x -= x*self.speed
-                self.y -= y*self.speed
-                return
+        dx=self.x+(x*self.speed)
+        dy=self.y+(y*self.speed)
+
+        if check_all_corner(dx,dy,self.Size_Tiles,self.Size_Tiles):
+            self.x=dx
+            self.y=dy
+            return
+
+        if check_all_corner(dx,self.y,self.Size_Tiles,self.Size_Tiles):
+            self.x=dx
+            return
+        
+        if check_all_corner(self.x,dy,self.Size_Tiles,self.Size_Tiles):
+            self.y=dy
+            return
+
+        
+
+# Blocks 
+
+player=block(10,5,2,0.2,'blue')
+enemy=block(20,20,2,0.2,'red')
+
+# functions 
+
+def show_screen():
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j]==1:
+                pygame.draw.rect(screen,'grey',(j*TILE_SIZE,i*TILE_SIZE,TILE_SIZE,TILE_SIZE))
+            else :
+                pygame.draw.rect(screen,'white',(j*TILE_SIZE,i*TILE_SIZE,TILE_SIZE,TILE_SIZE))
 
 
-# -------------------- BLOCK ------------------------
+def check_corner(x,y):
+    if grid[int(y)][int(x)]==0:
+        return True
+    else :
+        return False
+    
+def check_all_corner(x,y,w,h):
+    return check_corner(x,y) and check_corner(x+w,y) and check_corner(x,y+h) and check_corner(x+w,y+h)
 
-player=block(SCREEN_WIDTH//2,(3*SCREEN_HEIGHT)//4,40,40,3,'blue')
-enemy=block(SCREEN_WIDTH//2,SCREEN_HEIGHT//4,40,40,4,'red')
+def move_enemy():
 
-blocks=[]
-# --------boreder---------- 
-blocks.append(block(0,0,SCREEN_WIDTH,5,0,'grey'))
-blocks.append(block(0,0,5,SCREEN_HEIGHT,0,'grey'))
-blocks.append(block(0,SCREEN_HEIGHT-5,SCREEN_WIDTH,5,0,'grey'))
-blocks.append(block(SCREEN_WIDTH-5,0,5,SCREEN_HEIGHT,0,'grey'))
+    dx=player.x-enemy.x
+    dy=player.y-enemy.y
 
-# -----------objects---------------- 
+    dist=(dx**2+dy**2)**0.5
 
-blocks.append(block(100,100,50,150,0,'grey'))
-blocks.append(block(250,200,150,50,0,'grey'))
+    if dist!=0:
+        dx/=dist
+        dy/=dist
 
+    enemy.move(dx,dy)
 
-blocks.append(block(150, 50, 100, 30, 0, 'grey'))
-blocks.append(block(360, 180, 40, 120, 0, 'grey'))
+def check_enemy_collision():
 
+    player.w = player.Size_Tiles
+    player.h = player.Size_Tiles
+    enemy.w = enemy.Size_Tiles
+    enemy.h = enemy.Size_Tiles
 
-blocks.append(block(200, 420, 300, 30, 0, 'grey'))
-blocks.append(block(600, 250, 30, 120, 0, 'grey'))
+    if not(player.x+player.w<enemy.x or player.x>enemy.x+enemy.w or player.y+player.h<enemy.y or player.y>enemy.y+enemy.h):
+        global running
+        running=False
+        
 
+# Game Loop 
 
-# -------------------- GAME LOOP --------------------
-running = True
+running=True
+
 while running:
-    # --- Event Handling ---
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running=False
 
-    # --- Update (Game Logic) ---
-    # Put your update code here
+    # Game Logic 
     keys=pygame.key.get_pressed()
 
     if keys[pygame.K_a] or keys[pygame.K_LEFT]:
@@ -113,24 +150,19 @@ while running:
     if keys[pygame.K_s] or keys[pygame.K_DOWN]:
         player.move(0,1)
 
-    # if player will collide to enemy game will stop 
-    if collide(player,enemy):
-        running=False
+    move_enemy()
 
-    enemy_move(enemy,player)
+    check_enemy_collision()
 
-    # --- Draw (Render) ---
-    screen.fill(WHITE)       # Clear screen
+    # Draw 
+    show_screen()
     player.show()
     enemy.show()
 
-    for block in blocks:
-        block.show()
-    # Put your drawing code here
 
-    pygame.display.flip()    # Update display
-    clock.tick(FPS)          # Maintain FPS
-
+    pygame.display.update()
+    clock.tick(FPS)
 
 pygame.quit()
 sys.exit()
+        
